a1db8e3cfec2d898a0051b4257841cd9afeff015
PavelBortnovskiy <bortnovskiy.pavlo@gmail.com>
2024-03-06 14:57:32 +0200

    Basic security, validation, token generation, exception handling added

--- src/main/java/com/neo/mongocachetest/MongoCacheTestApplication.java
+++ src/main/java/com/neo/mongocachetest/MongoCacheTestApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import java.util.TimeZone;
 
 @EnableCaching
 @EnableScheduling
+@EnableWebSecurity
 @SpringBootApplication
 public class MongoCacheTestApplication {
 
